a5c99e378fda1032f8abdab657599973bdbea5cd
Maswadkar <maswadkar@gmail.com>
2019-02-17 14:15:44 +0000

    separated customers route from main file

--- server.js
+++ server.js
@@ -1,48 +1,16 @@
-const express = require('express');
-const mongoClient  = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-
-var coll = 'customer';
-var mydb='crm';
-var cursor;
-
+var PORT = 8800
 
 app = express();
 
+var customers = require('./src/customers')
+app.use('/customers',customers);
 
-
-
-app.listen(8800)
+app.listen(PORT)
 app.use(bodyParser.json());
 
 
-app.get('/ping',function(req,res){res.send('Pinging hallo world')});
-
-// Connect to the db
-mongoClient.connect('mongodb://mongodb-11:27017', {useNewUrlParser:true}, function(err, client) {
-  if(!err) {
-    cursor = client.db(mydb).collection(coll);
-  }
-});
-
-app.get('/customers',function(req,res){
-        cursor.find(req.query).toArray(function(err,documents){
-        res.json({customers:documents})});
-});
-
-app.post('/customers',function(req,res){
-    cursor.insertOne(req.body,function(err,result){
-        if(err){res.json(err)}
-        else(res.json({inserted_id:result.insertedId}));
-    });
-});
-
-
-app.delete('/customers',function(req,res){
-    cursor.deleteOne(req.query,function(err,result){
-        if(!err){res.json({result:result})}
-    });
-});
-
-
+app.get('/',function(req,res){res.json({homepage:'Welcome to the homepage of CRM restful services'})});
+app.get('/ping',function(req,res){res.json({message:'Pinging hallo world'})});
