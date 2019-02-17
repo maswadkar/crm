961c2842b557ff340ffc5587c551bba94931b5de
Maswadkar <maswadkar@gmail.com>
2019-02-17 14:33:25 +0000

    changed the git origin from https to git

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var PORT = 8800
-
 app = express();
+
 
 var customers = require('./src/customers')
 app.use('/customers',customers);
 
+
 app.listen(PORT)
 app.use(bodyParser.json());
-
 
 app.get('/',function(req,res){res.json({homepage:'Welcome to the homepage of CRM restful services'})});
 app.get('/ping',function(req,res){res.json({message:'Pinging hallo world'})});
