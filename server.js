d62ac4fc8be06f5c56aedcefdac611fa3fafe836
maswadkar <maswadkar@gmail.com>
2019-03-16 16:45:30 +0100

    removed the errors

--- server.js
+++ server.js
@@ -6,23 +6,20 @@ app = express();
 
 
 var customers = require('./src/customers')
-app.use('/customers',customers);
+app.use('/customers', customers);
 
 var album = require('./src/album')
-app.use('/albums',album);
+app.use('/albums', album);
 
+var director = require('./src/director')
+app.use('/directors', director);
 
-var album = require('./src/singer')
-app.use('/singers',singer);
-
-
-var album = require('./src/director')
-app.use('/directors',director);
-
+var singer = require('./src/singer')
+app.use('/singers', singer);
 
 
 app.listen(PORT)
 app.use(bodyParser.json());
 
-app.get('/',function(req,res){res.json({homepage:'Welcome to the homepage of CRM restful services'})});
-app.get('/ping',function(req,res){res.json({message:'Pinging hallo world'})});
+app.get('/', function (req, res) { res.json({ homepage: 'Welcome to the homepage of CRM restful services' }) });
+app.get('/ping', function (req, res) { res.json({ message: 'Pinging hallo world' }) });
--- src/album.js
+++ src/album.js
@@ -1,44 +1,47 @@
 var express = require('express');
-var mongoClient  = require('mongodb').MongoClient;
+var mongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 
 var coll = 'album';
-var mydb='crm';
+var mydb = 'crm';
 var cursor;
 
 var router = express.Router()
 
 
 // middleware that is specific to this router
-router.use(function timeLog (req, res, next) {
+router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now())
     next()
-  })
+})
 
 // Connect to the db
-mongoClient.connect('mongodb://mongodb-11:27017', {useNewUrlParser:true}, function(err, client) {
-  if(!err) {
-    cursor = client.db(mydb).collection(coll);
-  }
+mongoClient.connect('mongodb://mongodb-11:27017', { useNewUrlParser: true }, function (err, client) {
+    if (!err) {
+        cursor = client.db(mydb).collection(coll);
+    } else {
+        console.log('there was some issue in DB connection. Most likely you are not in the network')
+    }
 });
 
 // define the home page route
-router.get('/',function(req,res){
-        cursor.find(req.query).toArray(function(err,documents){
-        res.json({albums:documents})});
+router.get('/', function (req, res) {
+    cursor.find(req.query).toArray(function (err, documents) {
+        res.json({ albums: documents })
+    });
 });
 
-router.post('/',function(req,res){
-    cursor.insertOne(req.body,function(err,result){
-        if(err){res.json(err)}
-        else(res.json({inserted_id:result.insertedId}));
+router.post('/', function (req, res) {
+    cursor.insertOne(req.body, function (err, result) {
+        if (err) { res.json(err) }
+        else (res.json({ inserted_id: result.insertedId }));
     });
 });
 
 
-router.delete('/:id',function(req,res){
-    cursor.deleteOne(req.query,function(err,result){
-        if(!err){res.json({result:result})}
+router.delete('/:id', function (req, res) {
+    cursor.deleteOne(req.query, function (err, result) {
+        if (!err) { res.json({ result: result }) }
     });
 });
 
