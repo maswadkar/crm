7873c3d720da5f833169608f96cc623399fffd51
maswadkar <maswadkar@gmail.com>
2019-12-10 14:57:33 +0000

    added src db.js file

---
+++ src/db.js
@@ -0,0 +1,31 @@
+const assert = require("assert");
+const client = require("mongodb").MongoClient;
+let _db;
+
+module.exports = {
+    getDb,
+    initDb
+};
+
+
+function initDb(callback) {
+    if (_db) {
+        console.warn("Trying to init DB again!");
+        return callback(null, _db);
+    }
+
+client.connect('mongodb://admin:tiger@twitterx.organic-farmer.in:27017',{useNewUrlParser: true}, connected);
+function connected(err, db) {
+        if (err) {
+            return callback(err);
+        }
+        console.log("DB initialized - connected to: MONGODB - TWITTERX");
+        _db = db;
+        return callback(null, _db);
+    }
+}
+
+function getDb() {
+    assert.ok(_db, "Db has not been initialized. Please called init first.");
+    return _db;
+}
